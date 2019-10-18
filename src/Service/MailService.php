ee44b3fe2ab45f873cd96ba385d6dab045163ccf
Ingram <ingram.van.helvoort@mediamonks.com>
2019-10-18 17:02:17 +0200

    Commit version 1571410855222 at 2019-10-18 17:02:17

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -2,12 +2,16 @@
 
 namespace App\Service;
 
+use App\Entity\Profile;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Symfony\Component\HttpFoundation\Response;
 
 class MailService extends Client
 {
+
+    const TAG_APPROVED = 'photo_approved';
+    const TAG_DENIED = 'photo_denied';
 
     private $listId;
 
@@ -17,14 +21,41 @@ class MailService extends Client
         $this->listId = $listId;
     }
 
-    public function subscribeUser($email)
+    public function subscribeUser(Profile $profile)
+    {
+        $this->updateUser($profile->getEmail(), ['status' => 'subscribed']);
+    }
+
+    public function updateTag(Profile $profile, $tag)
+    {
+        $this->updateUser($profile->getEmail(), [
+            'merge_fields' => [
+                'UUID' => $profile->getUuid()
+            ]
+        ]);
+        try {
+            $this->post(sprintf('lists/%s/members/%s/tags', $this->listId, md5(strtolower($profile->getEmail()))), [
+                'body' => json_encode([
+                    'tags' => [
+                        [
+                            'name' => $tag,
+                            'status' => 'active'
+                        ]
+                    ]
+                ])
+            ]);
+        } catch(RequestException $e) {
+            var_dump($e->getMessage());exit;
+        }
+    }
+
+    public function updateUser($email, $fields = [])
     {
         try {
-            $this->post(sprintf('lists/%s/members', $this->listId), [
-                'body' => json_encode([
-                    'status' => 'subscribed',
+            $this->put(sprintf('lists/%s/members/%s', $this->listId, md5(strtolower($email))), [
+                'body' => json_encode(array_merge([
                     'email_address' => $email
-                ])
+                ], $fields))
             ]);
         } catch(RequestException $e) {
             if($e->getResponse()->getStatusCode() === Response::HTTP_BAD_REQUEST) {
